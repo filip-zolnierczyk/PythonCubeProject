0338b3e4472d4d55f286b7807db6e88fc573ec63
filip-zolnierczyk <filip.zolnierczyk@gmail.com>
2025-04-03 21:22:56 +0200

    przechowywanie 9 pol w stringu

--- tools.py
+++ tools.py
@@ -17,19 +17,33 @@ def opposite_side(x):
     else:
         throw_error("Color does not exist")
 
+
 def next_side(x):
     if x == "b":
         return "r"
     if x == "r":
         return "g"
     if x == "g":
-        return ("o")
+        return "o"
     if x == "o":
-        return ("b")
+        return "b"
     else:
         throw_error("Color does not exist")
 
+
+def previous_side(x):
+    if x == "b":
+        return "o"
+    if x == "o":
+        return "g"
+    if x == "g":
+        return "r"
+    if x == "r":
+        return "b"
+    else:
+        raise ValueError("Color does not exist")
+
+
 def convert_table_to_side(t):
-    center = t[4]
-    string = t[0]+t[3]+t[6]+t[1]+t[7]+t[2]+t[5]+t[8]
-    return (center, string)
+    string = t[0] + t[3] + t[6] + t[1] + t[4] + t[7] + t[2] + t[5] + t[8]
+    return string

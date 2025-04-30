df57e6dda0074ede0f6f6576b0cc3df391ee19c8
filip-zolnierczyk <filip.zolnierczyk@gmail.com>
2025-04-30 12:46:31 +0200

    rubiks

--- rubiks_data.py
+++ rubiks_data.py
@@ -1,5 +1,5 @@
-from setuptools.command.rotate import rotate
 from util.rubiks_move_util import *
+
 
 class RubiksCube:
     def __init__(self, tab=["ggggggggg", "rrrrrrrrr", "bbbbbbbbb", "ooooooooo", "yyyyyyyyy", "wwwwwwwww"]):
@@ -8,9 +8,9 @@ class RubiksCube:
         self.top_side = 'y'
         self.sides = {"g": "", "r": "", "b": "", "o": "", "y": "", "w": ""}
 
-        codes = ['g','r','b','o','y','w']
+        codes = ['g', 'r', 'b', 'o', 'y', 'w']
 
-        for i,t in enumerate(tab):
+        for i, t in enumerate(tab):
             self.sides[codes[i]] = t
 
     def rotate_face(self, side, clockwise=True):
@@ -57,50 +57,157 @@ class RubiksCube:
         else:
             raise ValueError(f"Nieznany ruch: {move_str}")
 
+    def rotate_cube(self, clockwise=True):
+        if clockwise:
+            self.facing_user = next_side(self.facing_user)
+            self.rotate_face(self.top_side, clockwise)
+            self.rotate_face(self.bottom_side, not clockwise)
+
+        if not clockwise:
+            for i in range(3): self.rotate_cube(True)
+
     #w przypadku kazdego ruchu trzeba zmienic indexację na kazdej sciance (bo sie obraca) i obrocic pozostale naklejki z 4 scian
     def perform_f_move(self, clockwise=True):
 
-        self.rotate_face(self.facing_user, clockwise)
-
         temp1 = self.sides[self.top_side][-3:]
-        temp2 = self.sides[next_side(self.facing_user)][0] + self.sides[next_side(self.facing_user)][3] + self.sides[next_side(self.facing_user)][6]
+        temp2 = self.sides[next_side(self.facing_user)][0] + self.sides[next_side(self.facing_user)][3] + \
+                self.sides[next_side(self.facing_user)][6]
         temp3 = self.sides[self.bottom_side][:3]
-        temp4 = self.sides[previous_side(self.facing_user)][2] + self.sides[previous_side(self.facing_user)][5] + self.sides[previous_side(self.facing_user)][8]
+        temp4 = self.sides[previous_side(self.facing_user)][8] + self.sides[previous_side(self.facing_user)][5] + \
+                self.sides[previous_side(self.facing_user)][2]
 
-        if clockwise: #chyba działa
+        if clockwise:  #działa
+
+            self.rotate_face(self.facing_user, clockwise)
 
             self.sides[self.top_side] = self.sides[self.top_side][:6] + temp4
 
-            print(self.sides[next_side(self.facing_user)])
-            self.sides[next_side(self.facing_user)] = temp1[0] + self.sides[next_side(self.facing_user)][1:3] + temp1[1] + self.sides[next_side(self.facing_user)][4:6] + temp1[2] + self.sides[next_side(self.facing_user)][7:9]
+            self.sides[next_side(self.facing_user)] = temp1[0] + self.sides[next_side(self.facing_user)][1:3] + temp1[
+                1] + self.sides[next_side(self.facing_user)][4:6] + temp1[2] + self.sides[next_side(self.facing_user)][
+                                                                               7:9]
 
             self.sides[self.bottom_side] = temp2[::-1] + self.sides[self.bottom_side][3:]
 
-            self.sides[previous_side(self.facing_user)] = self.sides[previous_side(self.facing_user)][0:2] + temp3[0] + self.sides[previous_side(self.facing_user)][3:5] + temp3[1] + self.sides[previous_side(self.facing_user)][6:8] + temp3[2]
+            self.sides[previous_side(self.facing_user)] = self.sides[previous_side(self.facing_user)][0:2] + temp3[0] + \
+                                                          self.sides[previous_side(self.facing_user)][3:5] + temp3[1] + \
+                                                          self.sides[previous_side(self.facing_user)][6:8] + temp3[2]
 
-        else: 
+        else:
             for i in range(3):
                 self.perform_f_move(self)
 
-
     def perform_r_move(self, clockwise=True):
-        self.rotate_face(next_side(self.facing_user), clockwise)
+        self.rotate_cube(True)
+        self.perform_f_move(clockwise)
+        self.rotate_cube(False)
 
     def perform_l_move(self, clockwise=True):
-        self.rotate_face(previous_side(self.facing_user), clockwise)
+        self.rotate_cube(False)
+        self.perform_f_move(clockwise)
+        self.rotate_cube(True)
 
     def perform_b_move(self, clockwise=True):
-        self.rotate_face(opposite_side(self.facing_user), clockwise)
+        for i in range(2): self.rotate_cube(True)
+        self.perform_f_move(clockwise)
+        for i in range(2): self.rotate_cube(True)
 
     def perform_u_move(self, clockwise=True):
-        pass
+
+        if clockwise:
+            self.rotate_face(self.top_side, clockwise)
+            face = self.sides[self.facing_user][:3]
+            right = self.sides[next_side(self.facing_user)][:3]
+            back = self.sides[next_side(next_side(self.facing_user))][:3]
+            left = self.sides[previous_side(self.facing_user)][:3]
+
+            self.sides[self.facing_user] = right + self.sides[self.facing_user][3:]
+            self.sides[next_side(self.facing_user)] = back + self.sides[next_side(self.facing_user)][3:]
+            self.sides[next_side(next_side(self.facing_user))] = left + self.sides[
+                                                                            next_side(next_side(self.facing_user))][3:]
+            self.sides[previous_side(self.facing_user)] = face + self.sides[previous_side(self.facing_user)][3:]
+
+        else:
+            for i in range(3):
+                self.perform_u_move(clockwise=True)
 
     def perform_d_move(self, clockwise=True):
-        pass
 
+        sides_keys = [self.facing_user,
+                      next_side(self.facing_user),
+                      next_side(next_side(self.facing_user)),
+                      previous_side(self.facing_user)]
+
+        # Pobieramy dolne trzy elementy z każdej ściany
+        rows = [self.sides[key][-3:] for key in sides_keys]
+
+        if clockwise:
+            self.rotate_face(self.bottom_side, clockwise)
+            for i in range(4):
+                key = sides_keys[(i + 1) % 4]
+                self.sides[key] = self.sides[key][:-3] + rows[i]
+        else:
+            for i in range(3):
+                self.perform_d_move(clockwise=True)
+
+    #do debugowania
     def display_cube(self):
-        for key, value in self.sides.items():
-            print(f"{key.upper()} side:")
-            for i in range(0, 9, 3):
-                print(value[i:i + 3])
-            print()
+        color_emojis = {
+            'R': '🔴', 'r': '🔴',
+            'G': '🟢', 'g': '🟢',
+            'B': '🔵', 'b': '🔵',
+            'O': '🟠', 'o': '🟠',
+            'Y': '🟡', 'y': '🟡',
+            'W': '⚪', 'w': '⚪',
+        }
+
+        def face_rows(face):
+            return [face[i:i + 3] for i in range(0, 9, 3)]
+
+        U = face_rows(self.sides['y'])
+        D = face_rows(self.sides['w'])
+        F = face_rows(self.sides[self.facing_user])
+        R = face_rows(self.sides[next_side(self.facing_user)])
+        B = face_rows(self.sides[next_side(next_side(self.facing_user))])
+        L = face_rows(self.sides[previous_side(self.facing_user)])
+
+        print("Up:")
+        for row in U:
+            print(' '.join(color_emojis[c] for c in row))
+        print()
+
+        print("Left | Front | Right | Back:")
+        for l, f, r, b in zip(L, F, R, B):
+            l_row = ' '.join(color_emojis[c] for c in l)
+            f_row = ' '.join(color_emojis[c] for c in f)
+            r_row = ' '.join(color_emojis[c] for c in r)
+            b_row = ' '.join(color_emojis[c] for c in b)
+            print(f"{l_row}   {f_row}   {r_row}   {b_row}")
+        print()
+
+        print("Down:")
+        for row in D:
+            print(' '.join(color_emojis[c] for c in row))
+
+    def perform_f2_move(self):
+        for i in range(2):
+            self.perform_f_move()
+
+    def perform_r2_move(self):
+        for i in range(2):
+            self.perform_r_move()
+
+    def perform_l2_move(self):
+        for i in range(2):
+            self.perform_l_move()
+
+    def perform_b2_move(self):
+        for i in range(2):
+            self.perform_b_move()
+
+    def perform_u2_move(self):
+        for i in range(2):
+            self.perform_u_move()
+
+    def perform_d2_move(self):
+        for i in range(2):
+            self.perform_d_move()
